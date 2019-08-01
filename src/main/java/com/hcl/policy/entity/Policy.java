ddcd68c21e4b3f988d4d2814923226fcf9175ab0
sontrive <sontrive0310@gmail.com>
2019-08-01 12:32:18 +0530

    commit

--- src/main/java/com/hcl/policy/entity/Policy.java
+++ src/main/java/com/hcl/policy/entity/Policy.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 import com.fasterxml.jackson.annotation.JsonIdentityInfo;
 import com.fasterxml.jackson.annotation.ObjectIdGenerators;
@@ -15,6 +16,7 @@ import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "policy")
 @JsonIdentityInfo(generator = ObjectIdGenerators.PropertyGenerator.class, property = "id")
 public class Policy implements Serializable {
 
@@ -28,5 +30,25 @@ public class Policy implements Serializable {
 	
 	@Column(name = "name")
 	private  String name;
+	
+	@Column(name = "entry_age")
+	private Integer entryAge;
+	
+	@Column(name = "max_maturity_age")
+	private Integer maxMaturityAge;
+	
+	@Column(name = "policy_term")
+	private Integer policyTerm;
+	
+	@Column(name = "min_remium")
+	private Double minPremium;
+	
+	@Column(name = "min_sum_assured")
+	private Double minSumAssured;
+	
+	@Column(name = "status")
+	private String status;
+	
+	
 
 }

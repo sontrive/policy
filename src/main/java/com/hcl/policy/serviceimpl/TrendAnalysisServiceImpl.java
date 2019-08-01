1830ea8d5b42236319730247cff878f5959e606d
Rajat <bigdatarajatpancholi@gmail.com>
2019-08-01 12:31:33 +0530

    setup

--- src/main/java/com/hcl/policy/serviceimpl/TrendAnalysisServiceImpl.java
+++ src/main/java/com/hcl/policy/serviceimpl/TrendAnalysisServiceImpl.java
@@ -1,7 +1,10 @@
 package com.hcl.policy.serviceimpl;
 
+import org.springframework.stereotype.Service;
+
 import com.hcl.policy.service.TrendAnalysisService;
 
+@Service
 public class TrendAnalysisServiceImpl implements TrendAnalysisService {
 
 }

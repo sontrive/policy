ee70d5d2f2da47fe0a536917246ed067110dc8c2
Rajat <bigdatarajatpancholi@gmail.com>
2019-08-01 12:29:10 +0530

    TrendAnalysisService

---
+++ src/main/java/com/hcl/policy/service/TrendAnalysisService.java
@@ -0,0 +1,5 @@
+package com.hcl.policy.service;
+
+public interface TrendAnalysisService {
+
+}
---
+++ src/main/java/com/hcl/policy/serviceimpl/TrendAnalysisServiceImpl.java
@@ -0,0 +1,7 @@
+package com.hcl.policy.serviceimpl;
+
+import com.hcl.policy.service.TrendAnalysisService;
+
+public class TrendAnalysisServiceImpl implements TrendAnalysisService {
+
+}

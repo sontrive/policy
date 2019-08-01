20dafde5ad178bdcc65532634bc7851646272d84
shivanjali.kanase <kanaseshivanjali@gmail.com>
2019-08-01 18:55:06 +0530

    initial commit.

--- src/main/java/com/hcl/policy/entity/User.java
+++ src/main/java/com/hcl/policy/entity/User.java
@@ -14,9 +14,6 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
-
 import lombok.Data;
 
 @Data

39f29f2e7605e369ba4daa87fd0172f99c36da5c
sontrive <sontrive0310@gmail.com>
2019-08-01 11:23:02 +0530

    commit

---
+++ src/main/java/com/hcl/policy/exception/ApplicationException.java
@@ -0,0 +1,21 @@
+package com.hcl.policy.exception;
+
+public class ApplicationException extends Exception{
+
+	private static final long serialVersionUID = 1L;
+	
+	public ApplicationException(Throwable t, String msg) {
+		super(msg, t);
+	}
+
+	public ApplicationException(Throwable t) {
+		super(t);
+	}
+
+	public ApplicationException(String msg) {
+		super(msg);
+	}
+
+	
+
+}
